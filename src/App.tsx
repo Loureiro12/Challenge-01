1c8bbe61dbcf3cce7cc65f9124ce21ba4f34b508
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-09-21 21:40:03 -0300

    feat: creating task header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import { Button } from "./components/Button";
 import { Header } from "./components/Header";
 import { InputAddTask } from "./components/InputAddTask";
+
+import styles from "./App.module.css";
+
 import "./global.css";
 
 function App() {
   return (
     <>
       <Header />
-      <Button />
-      <InputAddTask />
+      <div className={styles.wrapper}>
+        <InputAddTask />
+        <Button />
+      </div>
+      <div className={styles.tasks}>
+        <div className={styles.headerTasks}>
+          <p>
+            Tarefas criadas <span className={styles.counter}>5</span>
+          </p>
+          <p>
+            Concluídas <span className={styles.counter}>2 de 5</span>
+          </p>
+        </div>
+      </div>
     </>
   );
 }
